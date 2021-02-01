6a2f210cce73a072bb6e998b8ed82a5e51f43cba
Hakki SABAH <hakkisabah@hotmail.com.tr>
2021-02-02 00:48:45 +0300

    First commit from terminal

--- odev.js
+++ odev.js
@@ -1,56 +1,53 @@
-/** 
-
-1. mycarDetails fonksiyonunu "GA12345 Toyota" ciktisini verecek sekilde duzenleyin.
-
-**/
-
-var car = { 
+// Cevap 1
+/* Hakkı Sabah 02.02.2021
+*
+* Cevap olarak istenmediği için var let yapılmamıştır.
+* Sadece myCarDetails expression functionun 2. FAZI düzenlenmiştir.*/
+var car = {
     registrationNumber: "GA12345",
     brand: "Toyota",
 
-    displayDetails: function(){
+    displayDetails: function () {
         console.log(this.registrationNumber + " " + this.brand);
-    }
-}
+    },
+};
 
-var myCarDetails =  car.displayDetails;
+var myCarDetails = car.displayDetails.bind({
+    registrationNumber: car.registrationNumber,
+    brand: car.brand,
+});
 myCarDetails();
 
-
-/** 
-
-2.name parametresi alan bir isValidName fonksiyonu yazin. Fonksiyon asagidaki kosullarin hepsi saglaniyorsa true, saglanmiyorsa false donmelidir:
-
-name string olmali
-name bos olmamali
-bosluk icerebilir, ancak bosluk haridcindeki isimler en az 2 karakterden olusmali.
-
-**/
-
+// Cevap 2
+/* Hakkı Sabah 02.02.2021
+*
+* İlk önce string kontrol edilmeli eğer edilmez ise gönderilen sayı tipindeki argüman,
+* boşluk kontrolü yapılan name parametresinde hata verecektir.
+* Son olarak fonksiyonun istenen çıktıyı verdiğini gösteren 5 adet console.log() kullanımı hazırlanmıştır.
+* */
 function isValidName(name) {
-  /// your code here
+    return typeof name == "string" && name.replaceAll(" ", "").length >= 2
+        ? true
+        : false;
 }
+console.log(isValidName("Hakki")); // true
+console.log(isValidName("")); // false
+console.log(isValidName(" ")); // false
+console.log(isValidName("  ")); // false
+console.log(isValidName("  a")); // false
 
-
-
-/**
-
-
-3. summary fonkisyonunu ciktisi "Brave New World was written by Aldous Huxley. It is a dystopian novel written in 1932." olacak sekilde cagirin.
-
-**/
-
+// Cevap 3
+/* Hakkı Sabah 02.02.2021
+*
+* */
 const book = {
-  title: 'Brave New World',
-  author: 'Aldous Huxley',
-}
+    title: "Brave New World",
+    author: "Aldous Huxley",
+};
 
 function summary(genre, year) {
-  console.log(
-    `${this.title} was written by ${this.author}. It is a ${genre} novel written in ${year}.`,
-  )
+    console.log(
+        `${this.title} was written by ${this.author}. It is a ${genre} novel written in ${year}.`
+    );
 }
-
-
-
-
+summary.call(book, "dystopian", "1932");
